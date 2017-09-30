fa452d6107f8e6e677492c6516332e8ec640cc83
konez2k <konez2k@gmail.com>
2017-09-30 19:38:53 +0200

    missing error param

--- src/net.cpp
+++ src/net.cpp
@@ -901,6 +901,7 @@ void ThreadMapPort()
     devlist = upnpDiscover(2000, multicastif, minissdpdpath, 0, 0, &error);
 #else
     /* miniupnpc 1.9.20150730 */
+    int error = 0;
     devlist = upnpDiscover(2000, multicastif, minissdpdpath, 0, 0, 2, &error);
 #endif
 
