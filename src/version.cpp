00e4f429640c60ccd13f81cf12ed49998557bbfd
konez2k <konez2k@gmail.com>
2017-10-01 17:24:57 +0200

    removed inc

--- src/version.cpp
+++ src/version.cpp
@@ -36,7 +36,7 @@ const std::string CLIENT_NAME("Ember");
 // git will put "#define GIT_ARCHIVE 1" on the next line inside archives.
 #define GIT_ARCHIVE 1
 #ifdef GIT_ARCHIVE
-#    define GIT_COMMIT_ID "v1.1.3.0" // More informative with version number at this time.
+#    define GIT_COMMIT_ID "v1.1.3.1" // More informative with version number at this time.
 #    define GIT_COMMIT_DATE "Sep 15, 2017"
 #endif
 
